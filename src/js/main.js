8f36f05c449068e3673a925db689cc62efd978b4
Zakaria <zakaria.barkouche@gmail.com>
2021-08-29 22:15:35 +0100

    starting up the project

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+import "../sass/styles.scss";
+
+const toggleBtn = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav-top");
+const content = document.querySelector(".aside-content");
+const transText = document.querySelector(".translated-text");
+const transTextHeight = transText.scrollHeight;
+const link = document.querySelector(".showMore-link");
+
+link.addEventListener("click", () => {
+  content.classList.toggle("is-shown");
+  transText.style.height = `${transTextHeight}px`;
+
+  if (!content.classList.contains("is-shown")) {
+    transText.setAttribute("style", "height:0px");
+  }
+});
+
+toggleBtn.addEventListener("click", (e) => {
+  nav.classList.toggle("is-open");
+});
