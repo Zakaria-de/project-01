4bfc5828fe68b1581d5524e299f2f85fe598eda3
Zakaria <zakaria.barkouche@gmail.com>
2021-09-03 07:37:02 +0100

    added a js file (emailJs.js)

---
+++ src/js/emailJs.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    document.querySelector('.contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
+console.log('hello');
